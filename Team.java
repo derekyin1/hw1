b63af1d48a8b10201b98616991b541cecdffdd72
Derek <derekyin27@gmail.com>
2021-02-08 18:33:25 -0500

    working on methods

--- Team.java
+++ Team.java
@@ -3,19 +3,23 @@ public class Team{
   private Player[] players;
 
   public Team(){
-
+players = new Player[MAX_PLAYERS];
   }
 
   public Object clone(){
-
-  }
+Player[] clone = new Player[MAX_PLAYERS];
+for (int i = 0; i < MAX_PLAYERS; i++){
+  clone[i] = players[i];
+}
+return (Object) clone;
+}
 
   public boolean equals(Object obj){
-
+return false;
   }
 
   public int size(){
-
+return 0;
   }
 
   public void addPlayer(Player p, int position){
@@ -26,21 +30,21 @@ public class Team{
 
   }
 
-  public Player getPlayer(int position){
+  //public Player getPlayer(int position){
 
-  }
+//  }
 
-  public Player getLeader(String stat){
+  //public Player getLeader(String stat){
 
-  }
+  //}
 
   public void printAllPlayers(){
 
   }
 
-  public String toString(){
-    
-  }
+//  public String toString(){
+
+//  }
 
 
 }
