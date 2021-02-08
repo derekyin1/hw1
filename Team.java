a9d328fb366cc235e3199e8c128347d400d445f8
Derek <derekyin27@gmail.com>
2021-02-08 18:16:28 -0500

    methods

---
+++ Team.java
@@ -0,0 +1,46 @@
+public class Team{
+  public static final int MAX_PLAYERS = 40;
+  private Player[] players;
+
+  public Team(){
+
+  }
+
+  public Object clone(){
+
+  }
+
+  public boolean equals(Object obj){
+
+  }
+
+  public int size(){
+
+  }
+
+  public void addPlayer(Player p, int position){
+
+  }
+
+  public void removePlayer(int position){
+
+  }
+
+  public Player getPlayer(int position){
+
+  }
+
+  public Player getLeader(String stat){
+
+  }
+
+  public void printAllPlayers(){
+
+  }
+
+  public String toString(){
+    
+  }
+
+
+}
