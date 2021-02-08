dba35924066215dc5b92740d8f6e3efa5310f80b
Derek <derekyin27@gmail.com>
2021-02-08 17:21:21 -0500

    accessor and mutator

--- homework.java
+++ homework.java
@@ -1,3 +1,35 @@
 public class Player{
+private String name;
+private int numHits;
+private int numErrors;
+
+public Player(){
+
+}
+
+public String getName(){
+  return name;
+}
+
+public int getNumHits(){
+  return numHits;
+}
+
+public int numErrors(){
+  return numErrors;
+}
+
+public void setName(String name1){
+  name = name1;
+}
+
+public void setNumHits(int numHits1){
+  numHits = numHits1;
+}
+
+public void setNumErrors(int numErrors1){
+  numErrors = numErrors1;
+}
+
 
 }
