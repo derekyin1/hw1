103738885f46c55b2f153cc8dd73ba4107eb5b36
Derek <derekyin27@gmail.com>
2021-02-12 16:54:48 -0500

    working on all methods

--- Team.java
+++ Team.java
@@ -1,35 +1,36 @@
+//Derek Yin 113251504 Recitation Section 1
 public class Team{
   public static final int MAX_PLAYERS = 40;
+  private int manyItems;
   private Player[] players;
 
   public Team(){
-players = new Player[MAX_PLAYERS];
+    players = new Player[MAX_PLAYERS];
+    manyItems = 0;
   }
 
   public Object clone(){
-Player[] clone = new Player[MAX_PLAYERS];
-for (int i = 0; i < MAX_PLAYERS; i++){
-  clone[i] = players[i];
-}
-return (Object) clone;
-}
+    Object copy = new Team();
+    for (int i = 0; i < manyItems; i++){
+
+
+    }
+  }
 
   public boolean equals(Object obj){
-    for(int i = 0; i < MAX_PLAYERS; i++){
-      if (((Player[]) obj)[i].getName().equals(players[i].getName())){
-        if ((((Player[])obj)[i].getNumHits() != players[i].getNumHits()) || (((Player[])obj)[i].getNumErrors() != players[i].getNumErrors())){
-          return false;
-        }
-      }
-    }
-return true;
+  if (obj instanceof Player){
+    Player p = (Player) obj;
+    return this.getNumHits() == p.getNumHits() && this.getNumErrors() == p.getNumErrors() && this.getName().equals(p.getName());
+  }
+  return false;
   }
 
   public int size(){
-return 0;
+    return manyItems;
   }
 
   public void addPlayer(Player p, int position){
+
 
   }
 
@@ -37,13 +38,36 @@ return 0;
 
   }
 
-  //public Player getPlayer(int position){
+  public Player getPlayer(int position){
+    if (position > manyItems || position-1 < 0){
+      throw new IllegalArgumentException("Invalid position");
+    }
+    else return players[position-1];
 
-//  }
+  }
 
-  //public Player getLeader(String stat){
+  public Player getLeader(String stat){
+    if (stat.equals("hits")){
+      Player currentBest = players[0];
+      for (int i = 0; i < manyItems; i++){
+        if (players[i].getNumHits() > currentBest.getNumHits()){
+          currentBest = players[i];
+        }
+      }
+      return currentBest;
+    }
+    if (stat.equals("errors")){
+      Player currentBest = players[0];
+      for (int i = 0; i < manyItems; i++){
+        if (players[i].getNumErrors() < currentBest.getNumErrors()){
+          currentBest = players[i];
+        }
+      }
+      return currentBest;
+    }
+    else throw IllegalArgumentException("No such statistic.");
 
-  //}
+  }
 
   public void printAllPlayers(){
 
--- player.java
+++ player.java
@@ -1,47 +1,48 @@
+//Derek Yin 113251504 Recitation Section 1
 public class Player{
-private String name;
-private int numHits;
-private int numErrors;
+  private String name;
+  private int numHits;
+  private int numErrors;
 
-public Player(){
+  public Player(){
 
-}
-
-public String getName(){
-  return name;
-}
-
-public int getNumHits(){
-  return numHits;
-}
-
-public int getNumErrors(){
-  return numErrors;
-}
-
-public void setName(String name1){
-  name = name1;
-}
-
-public void setNumHits(int numHits1){
-  if (numHits1 < 0){
-    throw new IllegalArgumentException();
   }
-  numHits = numHits1;
-}
 
-public void setNumErrors(int numErrors1){
-  if (numErrors1 < 0){
-    throw new IllegalArgumentException();
+  public String getName(){
+    return name;
   }
-  numErrors = numErrors1;
-}
 
-public String toString(){
-  return name + ", " + String.valueOf(numHits) + ", " + String.valueOf(numErrors);
-}
-public static void main(String[] args){
-  Player test = new Player();
-  System.out.println(test.toString());
-}
+  public int getNumHits(){
+    return numHits;
+  }
+
+  public int getNumErrors(){
+    return numErrors;
+  }
+
+  public void setName(String name1){
+    name = name1;
+  }
+
+  public void setNumHits(int numHits1){
+    if (numHits1 < 0){
+      throw new IllegalArgumentException();
+    }
+    numHits = numHits1;
+  }
+
+  public void setNumErrors(int numErrors1){
+    if (numErrors1 < 0){
+      throw new IllegalArgumentException();
+    }
+    numErrors = numErrors1;
+  }
+
+  public String toString(){
+    return name + ", " + String.valueOf(numHits) + ", " + String.valueOf(numErrors);
+  }
+  public static void main(String[] args){
+    Player test = new Player();
+    System.out.println(test.toString());
+  }
 }
